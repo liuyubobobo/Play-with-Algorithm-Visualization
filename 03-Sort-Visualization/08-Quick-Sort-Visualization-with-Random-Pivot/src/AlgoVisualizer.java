cdb0b9df4a4e755ec800e8a963606ba75a0df52b
liuyubobobo <liuyubobobo@gmail.com>
2017-07-12 17:59:12 -0700

    Chapter 03 section 08 code added.

---
+++ 03-Sort-Visualization/08-Quick-Sort-Visualization-with-Random-Pivot/src/AlgoVisualizer.java
@@ -0,0 +1,109 @@
+import java.awt.*;
+import java.util.Random;
+import java.util.Arrays;
+
+public class AlgoVisualizer {
+
+    private static int DELAY = 40;
+
+    private QuickSortData data;
+    private AlgoFrame frame;
+
+    public AlgoVisualizer(int N, AlgoFrame frame, QuickSortData data){
+
+        this.frame = frame;
+        this.data = data;
+    }
+
+    public void run(){
+
+        this.setData(-1, -1, -1, -1, -1);
+        AlgoVisHelper.pause(DELAY);
+
+        quickSort(0, data.N()-1);
+
+        this.setData(0, data.N()-1, -1, -1, -1);
+        AlgoVisHelper.pause(DELAY);
+    }
+
+    private void quickSort(int l, int r){
+
+//        if( l >= r )
+//            return;
+
+        if( l > r )
+            return;
+
+        if( l == r ){
+            setData(l, r, l, -1, -1);
+            AlgoVisHelper.pause(DELAY);
+            return;
+        }
+
+        setData(l, r, -1, -1, -1);
+        AlgoVisHelper.pause(DELAY);
+
+        int p = partition(l, r);
+        quickSort(l, p-1 );
+        quickSort(p+1, r);
+    }
+
+    private int partition(int l, int r){
+
+        int p = (int)(Math.random()*(r-l+1)) + l;
+        data.swap(l, p);
+        setData(l, r, -1, l, p);
+        AlgoVisHelper.pause(DELAY);
+
+        int v = data.get(l);
+        setData(l, r, -1, l, -1);
+        AlgoVisHelper.pause(DELAY);
+
+        int j = l; // arr[l+1...j] < v ; arr[j+1...i) > v
+        for( int i = l + 1 ; i <= r ; i ++ ) {
+            setData(l, r, -1, l, i);
+            AlgoVisHelper.pause(DELAY);
+            if (data.get(i) < v) {
+                j++;
+                data.swap(j, i);
+                setData(l, r, -1, l, i);
+                AlgoVisHelper.pause(DELAY);
+            }
+        }
+
+        data.swap(l, j);
+        setData(l, r, j, -1, -1);
+        AlgoVisHelper.pause(DELAY);
+
+        return j;
+    }
+
+    private void setData(int l, int r, int fixedPivot, int curPivot, int curElement){
+        data.l = l;
+        data.r = r;
+        if(fixedPivot != -1)
+            data.fixedPivots[fixedPivot] = true;
+        data.curPivot = curPivot;
+        data.curElement = curElement;
+        frame.setData(data);
+    }
+
+    public static void main(String[] args) {
+
+        int sceneWidth = 800;
+        int sceneHeight = 800;
+
+        EventQueue.invokeLater(() -> {
+            AlgoFrame frame = new AlgoFrame("Quick Sort Visualization", sceneWidth,sceneHeight);
+
+            int N = 200;
+            // int N = 100;
+
+            QuickSortData data = new QuickSortData(N, sceneHeight, true);
+            AlgoVisualizer vis = new AlgoVisualizer(N, frame, data);
+            new Thread(() -> {
+                vis.run();
+            }).start();
+        });
+    }
+}
---
+++ 03-Sort-Visualization/08-Quick-Sort-Visualization-with-Random-Pivot/src/QuickSortData.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+import java.util.Random;
+
+
+public class QuickSortData {
+
+    private int N;
+
+    public int[] numbers;
+    public int l, r;
+    public boolean[] fixedPivots;
+    public int curPivot;
+    public int curElement;
+
+    // 生成N个[0,randomBound)之间的随机数；nearlyOrdered控制随机数是否近乎有序
+    public QuickSortData(int N, int randomBound, boolean nearlyOrdered){
+        this.N = N;
+
+        numbers = new int[N];
+        fixedPivots = new boolean[N];
+
+        Random rand = new Random();
+        for( int i = 0 ; i < N ; i ++) {
+            numbers[i] = rand.nextInt(randomBound) + 1;
+            fixedPivots[i] = false;
+        }
+
+        if(nearlyOrdered){
+            Arrays.sort(numbers);
+            int swapTime = (int)(0.02*N);
+            for(int i = 0 ; i < swapTime; i ++){
+                int a = rand.nextInt(N);
+                int b = rand.nextInt(N);
+                swap(a, b);
+            }
+        }
+    }
+
+    // 生成N个[lBound,rBound]之间的随机数
+    public QuickSortData(int N, int lBound, int rBound){
+        this.N = N;
+
+        numbers = new int[N];
+
+        Random rand = new Random();
+        for( int i = 0 ; i < N ; i ++)
+            numbers[i] = rand.nextInt(rBound-lBound+1) + lBound;
+    }
+
+    public int N(){
+        return N;
+    }
+
+    public int get(int index){
+        if( index < 0 || index >= numbers.length)
+            throw new IllegalArgumentException("Invalid index to access Selection Sort Data.");
+
+        return numbers[index];
+    }
+
+    public void swap(int i, int j) {
+        int t = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = t;
+    }
+}
