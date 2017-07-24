988b22c72752e2ab486b5ab35dee3818094de498
liuyubobobo <liuyubobobo@gmail.com>
2017-07-24 15:44:10 -0700

    Chapter 07 section 08 codes added.

---
+++ 07-Move-the-Box-Solver/08-Using-HashSet/src/GameData.java
@@ -0,0 +1,144 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+
+public class GameData {
+
+    private int maxTurn;
+    private int N, M;
+    private Board starterBoard;
+    private Board showBoard;
+    private HashSet<Board> searchedBoards;
+
+    public int clickx, clicky;
+
+    public GameData(String filename){
+
+        if(filename == null)
+            throw new IllegalArgumentException("Filename cannot be null!");
+
+        Scanner scanner = null;
+        try{
+            File file = new File(filename);
+            if(!file.exists())
+                throw new IllegalArgumentException("File " + filename + " doesn't exist!");
+
+            FileInputStream fis = new FileInputStream(file);
+            scanner = new Scanner(new BufferedInputStream(fis), "UTF-8");
+
+            String turnline = scanner.nextLine();
+
+            this.maxTurn = Integer.parseInt(turnline);
+
+            ArrayList<String> lines = new ArrayList<String>();
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                lines.add(line);
+            }
+            starterBoard = new Board(lines.toArray(new String[lines.size()]));
+
+            this.N = starterBoard.N();
+            this.M = starterBoard.M();
+
+            //starterBoard.print();
+
+            showBoard = new Board(starterBoard);
+
+            searchedBoards = new HashSet<Board>();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        finally {
+            if(scanner != null)
+                scanner.close();
+        }
+    }
+
+    public int N(){ return N; }
+    public int M(){ return M; }
+    public Board getShowBoard(){ return showBoard;}
+
+    public boolean inArea(int x, int y){
+        return x >= 0 && x < N && y >= 0 && y < M;
+    }
+
+    public boolean solve(){
+
+        if(maxTurn < 0)
+            return false;
+
+        long startTime = System.currentTimeMillis();
+
+        searchedBoards.add(starterBoard);
+        boolean ret = solve(new Board(starterBoard), maxTurn);
+
+        long endTime = System.currentTimeMillis();
+        System.out.println( "Time : " + (endTime-startTime) + "ms" );
+
+        return ret;
+    }
+
+    private int d[][] = {{-1, 0}, {0, -1}, {0, 1}};
+    private boolean solve(Board board, int turn){
+
+        if(board == null)
+            throw new IllegalArgumentException("board can not be null in solve function!");
+
+//        System.out.println("= turn " + turn + " =");
+//        board.print();
+//        System.out.println("==========");
+
+        if(turn == 0)
+            return isWin(board);
+
+        if(isWin(board))
+            return true;
+
+        for(int x = 0 ; x < N ; x ++)
+            for(int y = 0 ; y < M ; y ++)
+                if(board.getData(x, y) != Board.EMPTY){
+                    for(int i = 0 ; i < 3 ; i ++){
+                        int newX = x + d[i][0];
+                        int newY = y + d[i][1];
+                        if(inArea(newX, newY)){
+                            Board nextBoard = new Board(board);
+                            nextBoard.preBoard = board;
+                            nextBoard.swapString = String.format("swap (%d, %d) and (%d, %d)", x, y, newX, newY);
+                            nextBoard.swap(x, y, newX, newY);
+                            nextBoard.run();
+
+                            if(!searchedBoards.contains(nextBoard)){
+                                searchedBoards.add(nextBoard);
+                                if(solve(nextBoard, turn-1))
+                                    return true;
+                            }
+
+                        }
+                    }
+                }
+
+        return false;
+    }
+
+    private boolean isWin(Board board){
+
+        if(board == null)
+            throw new IllegalArgumentException("board can not be null in isWin function!");
+
+        for(int i = 0 ; i < N ; i ++)
+            for(int j = 0 ; j < M ; j ++)
+                if(board.getData(i, j) != Board.EMPTY)
+                    return false;
+
+        board.printSwapInfo();
+
+        return true;
+    }
+}
