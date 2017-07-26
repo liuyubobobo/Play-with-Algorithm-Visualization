1379691a9933dd4443328f68f65b03a2a73e675a
liuyubobobo <liuyubobobo@gmail.com>
2017-07-26 16:10:12 -0700

    Chapter 08 section 07 codes added.

---
+++ 08-Fractal-Drawing/07-Snowflake/src/AlgoFrame.java
@@ -0,0 +1,113 @@
+import java.awt.Graphics2D;
+import java.awt.Graphics;
+import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.RenderingHints;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+import javax.swing.*;
+
+public class AlgoFrame extends JFrame{
+
+    private int canvasWidth;
+    private int canvasHeight;
+    private JPanel canvas;
+
+    public AlgoFrame(String title, int canvasWidth, int canvasHeight){
+
+        super(title);
+
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+
+        AlgoCanvas canvas = new AlgoCanvas();
+        setContentPane(canvas);
+        pack();
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        setVisible(true);
+    }
+
+    public AlgoFrame(String title){
+
+        this(title, 1024, 768);
+    }
+
+    public int getCanvasWidth(){return canvasWidth;}
+    public int getCanvasHeight(){return canvasHeight;}
+
+    // data
+    FractalData data;
+    public void setData(FractalData data){
+        this.data = data;
+        repaint();
+    }
+
+    private class AlgoCanvas extends JPanel{
+
+        public AlgoCanvas(){
+            // 双缓存
+            super(true);
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            Graphics2D g2d = (Graphics2D)g;
+
+            // 抗锯齿
+            RenderingHints hints = new RenderingHints(
+                    RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+            hints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            g2d.addRenderingHints(hints);
+
+            // 具体绘制
+            drawFractal(g2d, 0, canvasHeight-3, canvasWidth, 0, 0);
+        }
+
+        private void drawFractal(Graphics2D g, double x1, double y1, double side, double angle, int depth){
+
+            if(side <= 0)
+                return;
+
+            if( depth == data.depth ){
+                double x2 = x1 + side * Math.cos(angle*Math.PI/180.0);
+                double y2 = y1 - side * Math.sin(angle*Math.PI/180.0);
+                AlgoVisHelper.setColor(g, AlgoVisHelper.Indigo);
+                AlgoVisHelper.drawLine(g, x1, y1, x2, y2);
+                return;
+            }
+
+            double side_3 = side / 3;
+
+            double x2 = x1 + side_3 * Math.cos(angle*Math.PI/180.0);
+            double y2 = y1 - side_3 * Math.sin(angle*Math.PI/180.0);
+            drawFractal(g, x1, y1, side_3, angle, depth+1);
+
+            double x3 = x2 + side_3 * Math.cos((angle+60.0)*Math.PI/180.0);
+            double y3 = y2 - side_3 * Math.sin((angle+60.0)*Math.PI/180.0);
+            drawFractal(g, x2, y2, side_3, angle+60.0, depth+1);
+
+            double x4 = x3 + side_3 * Math.cos((angle-60.0)*Math.PI/180.0);
+            double y4 = y3 - side_3 * Math.sin((angle-60.0)*Math.PI/180.0);
+            drawFractal(g, x3, y3, side_3, angle-60.0, depth+1);
+
+            double x5 = x4 + side_3 * Math.cos(angle*Math.PI/180.0);
+            double y5 = y4 - side_3 * Math.sin(angle*Math.PI/180.0);
+            drawFractal(g, x4, y4, side_3, angle, depth+1);
+
+            return;
+        }
+
+        @Override
+        public Dimension getPreferredSize(){
+            return new Dimension(canvasWidth, canvasHeight);
+        }
+    }
+}
---
+++ 08-Fractal-Drawing/Snowflake/src/AlgoVisualizer.java
@@ -0,0 +1,67 @@
+import javafx.scene.input.MouseButton;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class AlgoVisualizer {
+
+    private static int DELAY = 40;
+
+    private FractalData data;
+    private AlgoFrame frame;
+
+    public AlgoVisualizer(AlgoFrame frame, FractalData data){
+
+        this.frame = frame;
+        this.data = data;
+        this.setData(data);
+    }
+
+    public void run(){
+
+        this.setData(data);
+        AlgoVisHelper.pause(DELAY);
+    }
+
+    private void setData(FractalData data){
+        frame.setData(data);
+    }
+
+    public void addAlgoKeyListener(){
+        frame.addKeyListener(new AlgoKeyListener());
+    }
+
+    private class AlgoKeyListener extends KeyAdapter{
+
+        @Override
+        public void keyReleased(KeyEvent event){
+            //System.out.println("Key released:" + event);
+            if(event.getKeyChar() >= '0' && event.getKeyChar() <= '9'){
+                int depth = event.getKeyChar() - '0';
+                data.depth = depth;
+                setData(data);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int depth = 6;
+        int side = 300;
+        int sceneWidth = 3*side+3;
+        int sceneHeight = side;
+
+        EventQueue.invokeLater(() -> {
+            AlgoFrame frame = new AlgoFrame("Fractal Visualizer", sceneWidth,sceneHeight);
+
+            FractalData data = new FractalData(depth);
+
+            AlgoVisualizer vis = new AlgoVisualizer(frame, data);
+            vis.addAlgoKeyListener();
+            new Thread(() -> {
+                vis.run();
+            }).start();
+        });
+    }
+}
