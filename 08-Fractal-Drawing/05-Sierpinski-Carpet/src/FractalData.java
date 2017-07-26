5854b2bb841fc1c0e14a36e6a478673bed9dd3b3
liuyubobobo <liuyubobobo@gmail.com>
2017-07-26 13:44:52 -0700

    Chapter 08 section 05 codes added.

---
+++ 08-Fractal-Drawing/05-Sierpinski-Carpet/src/FractalData.java
@@ -0,0 +1,10 @@
+
+public class FractalData {
+
+    public int depth;
+
+    public FractalData(int depth){
+        this.depth = depth;
+    }
+
+}
---
+++ 08-Fractal-Drawing/Sierpinski-Triangle/src/AlgoFrame.java
@@ -0,0 +1,106 @@
+import java.awt.Graphics2D;
+import java.awt.Graphics;
+import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.RenderingHints;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+import javax.swing.*;
+
+public class AlgoFrame extends JFrame{
+
+    private int canvasWidth;
+    private int canvasHeight;
+    private JPanel canvas;
+
+    public AlgoFrame(String title, int canvasWidth, int canvasHeight){
+
+        super(title);
+
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+
+        AlgoCanvas canvas = new AlgoCanvas();
+        setContentPane(canvas);
+        pack();
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        setVisible(true);
+    }
+
+    public AlgoFrame(String title){
+
+        this(title, 1024, 768);
+    }
+
+    public int getCanvasWidth(){return canvasWidth;}
+    public int getCanvasHeight(){return canvasHeight;}
+
+    // data
+    FractalData data;
+    public void setData(FractalData data){
+        this.data = data;
+        repaint();
+    }
+
+    private class AlgoCanvas extends JPanel{
+
+        public AlgoCanvas(){
+            // 双缓存
+            super(true);
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            Graphics2D g2d = (Graphics2D)g;
+
+            // 抗锯齿
+            RenderingHints hints = new RenderingHints(
+                    RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+            hints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            g2d.addRenderingHints(hints);
+
+            // 具体绘制
+            drawFractal(g2d, 0, 0, canvasWidth, canvasHeight, 0);
+        }
+
+        private void drawFractal(Graphics2D g, int x, int y, int w, int h, int depth){
+
+            int w_3 = w/3;
+            int h_3 = h/3;
+
+            if(w_3 <= 0 || h_3 <= 0)
+                return;
+
+            if( depth == data.depth ){
+                AlgoVisHelper.setColor(g, AlgoVisHelper.Indigo);
+                AlgoVisHelper.fillRectangle(g, x+w_3, y+h_3, w_3, h_3);
+                return;
+            }
+
+
+            for(int i = 0 ; i < 3 ; i ++)
+                for(int j = 0 ; j < 3 ; j ++)
+                    if(i == 1 && j == 1){
+                        AlgoVisHelper.setColor(g, AlgoVisHelper.Indigo);
+                        AlgoVisHelper.fillRectangle(g, x+i*w_3, y+j*h_3, w_3, h_3);
+                    }
+                    else
+                        drawFractal(g, x+i*w_3, y+j*h_3, w_3, h_3, depth+1);
+
+            return;
+        }
+
+        @Override
+        public Dimension getPreferredSize(){
+            return new Dimension(canvasWidth, canvasHeight);
+        }
+    }
+}
