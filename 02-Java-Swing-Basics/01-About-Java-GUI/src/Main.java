ce31a60f8fc42baf59a64947609bc9ce074fa423
liuyubobobo <liuyubobobo@gmail.com>
2017-08-20 03:14:16 -0700

    Chapter 02 section 01 codes added.

---
+++ 02-Java-Swing-Basics/01-About-Java-GUI/src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            JFrame frame = new JFrame();
+            frame.setSize(500, 500);
+            frame.setResizable(false);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}
