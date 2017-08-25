6134fe5a5ef6de369dc33aa464f67fc8d3c71215
liuyubobobo <liuyubobobo@gmail.com>
2017-08-25 11:22:19 -0700

    Chapter 03 section 06 updated.

--- 03-Probability-Simulation/06-Three-Gates-Problem/src/ThreeGatesExperiment.java
+++ 03-Probability-Simulation/06-Three-Gates-Problem/src/ThreeGatesExperiment.java
@@ -17,16 +17,18 @@ public class ThreeGatesExperiment {
         for(int i = 0 ; i < N ; i ++)
             if(play(changeDoor))
                 wins ++;
+
         System.out.println(changeDoor ? "Change" : "Not Change");
-        System.out.println("winning rate:" + (double)wins/N);
+        System.out.println("winning rate: " + (double)wins/N);
     }
 
     private boolean play(boolean changeDoor){
 
+        // Door 0, 1, 2
         int prizeDoor = (int)(Math.random() * 3);
         int playerChoice = (int)(Math.random() * 3);
 
-        if(playerChoice == prizeDoor)
+        if( playerChoice == prizeDoor)
             return changeDoor ? false : true;
         else
             return changeDoor ? true : false;
@@ -34,7 +36,7 @@ public class ThreeGatesExperiment {
 
     public static void main(String[] args) {
 
-        int N = 1000000;
+        int N = 10000000;
         ThreeGatesExperiment exp = new ThreeGatesExperiment(N);
 
         exp.run(true);
