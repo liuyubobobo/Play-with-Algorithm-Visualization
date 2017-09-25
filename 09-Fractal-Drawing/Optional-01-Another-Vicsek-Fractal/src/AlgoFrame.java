e279e87275c649cf1a6aca0063ac39a55d2db63c
liuyubobobo <liuyubobobo@gmail.com>
2017-09-25 13:43:42 -0700

    Vicsek Fractal Drawing codes updated.

---
+++ 09-Fractal-Drawing/Optional-01-Another-Vicsek-Fractal/src/AlgoFrame.java
@@ -0,0 +1,99 @@
+import java.awt.Graphics2D;
+import java.awt.Graphics;
+import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.RenderingHints;
+import javax.swing.*;
+
+public class AlgoFrame extends JFrame{
+
+    private int canvasWidth;
+    private int canvasHeight;
+
+    public AlgoFrame(String title, int canvasWidth, int canvasHeight){
+
+        super(title);
+
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+
+        AlgoCanvas canvas = new AlgoCanvas();
+        setContentPane(canvas);
+        pack();
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        setVisible(true);
+    }
+
+    public AlgoFrame(String title){
+
+        this(title, 1024, 768);
+    }
+
+    public int getCanvasWidth(){return canvasWidth;}
+    public int getCanvasHeight(){return canvasHeight;}
+
+    // data
+    private FractalData data;
+    public void render(FractalData data){
+        this.data = data;
+        repaint();
+    }
+
+    private class AlgoCanvas extends JPanel{
+
+        public AlgoCanvas(){
+            // 双缓存
+            super(true);
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            Graphics2D g2d = (Graphics2D)g;
+
+            // 抗锯齿
+            RenderingHints hints = new RenderingHints(
+                    RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+            hints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            g2d.addRenderingHints(hints);
+
+            // 具体绘制
+            drawFractal(g2d, 0, 0, canvasWidth, canvasHeight, 0);
+        }
+
+        private void drawFractal(Graphics2D g, int x, int y, int w, int h, int depth){
+
+            if( depth == data.depth ){
+                AlgoVisHelper.setColor(g, AlgoVisHelper.Indigo);
+                AlgoVisHelper.fillRectangle(g, x, y, w, h);
+                return;
+            }
+
+            if(w <= 1 || h <= 1){
+                AlgoVisHelper.setColor(g, AlgoVisHelper.Indigo);
+                AlgoVisHelper.fillRectangle(g, x, y, Math.max(w,1), Math.max(h,1));
+                return;
+            }
+
+            int w_3 = w/3;
+            int h_3 = h/3;
+            drawFractal(g, x + w_3, y, w_3, h_3, depth + 1);
+            drawFractal(g, x, y + h_3, w_3, h_3, depth + 1);
+            drawFractal(g, x + w_3, y + h_3, w_3, h_3, depth + 1);
+            drawFractal(g, x + 2 * w_3, y + h_3, w_3, h_3, depth + 1);
+            drawFractal(g, x + w_3, y + 2 * h_3, w_3, h_3, depth + 1);
+
+            return;
+        }
+
+        @Override
+        public Dimension getPreferredSize(){
+            return new Dimension(canvasWidth, canvasHeight);
+        }
+    }
+}
---
+++ 09-Fractal-Drawing/Optional-01-Another-Vicsek-Fractal/src/AlgoVisualizer.java
@@ -0,0 +1,60 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class AlgoVisualizer {
+
+    private static int DELAY = 40;
+
+    private FractalData data;
+    private AlgoFrame frame;
+
+    public AlgoVisualizer(int maxDepth){
+
+        data = new FractalData(maxDepth);
+        int sceneWidth = (int)Math.pow(3, maxDepth);
+        int sceneHeight = (int)Math.pow(3, maxDepth);
+
+        EventQueue.invokeLater(() -> {
+            frame = new AlgoFrame("Fractal Visualizer", sceneWidth,sceneHeight);
+            frame.addKeyListener(new AlgoKeyListener());
+            new Thread(() -> {
+                run();
+            }).start();
+        });
+    }
+
+    private void run(){
+
+        setData(data.depth);
+    }
+
+    private void setData(int depth){
+        data.depth = depth;
+
+        frame.render(data);
+        AlgoVisHelper.pause(DELAY);
+    }
+
+    public void addAlgoKeyListener(){
+        frame.addKeyListener(new AlgoKeyListener());
+    }
+
+    private class AlgoKeyListener extends KeyAdapter{
+
+        @Override
+        public void keyReleased(KeyEvent event){
+            //System.out.println("Key released:" + event);
+            if(event.getKeyChar() >= '0' && event.getKeyChar() <= '9'){
+                int depth = event.getKeyChar() - '0';
+                setData(depth);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int maxDepth = 6;
+
+        AlgoVisualizer vis = new AlgoVisualizer(maxDepth);
+    }
+}
